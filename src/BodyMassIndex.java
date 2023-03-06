ece423a248feb77fb5c557ae75f35838ea5ca2fc
KatiaKost <katya-donya@mail.ru>
2023-03-06 23:27:31 +1000

    remade homework v2

--- src/BodyMassIndex.java
+++ src/BodyMassIndex.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class BodyMassIndex {
+import java.util.Scanner;
+
+public class BodyMassIndex {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Введите свой рост в дюймах: ");
+        double heightInInches = input.nextDouble();
+        System.out.println("Введите свой вес в фунтах: ");
+        double weightInPounds = input.nextDouble();
+
+        final double METERS_OF_INCHES = 0.0254;
+        double heightInMeters = heightInInches * METERS_OF_INCHES;
+        final double KILOGRAMS_OF_POUNDS = 0.45359237;
+        double weightInKilograms = weightInPounds * KILOGRAMS_OF_POUNDS;
+        double bodyMassIndex = weightInKilograms / (heightInMeters * heightInMeters);
+        System.out.println("Индекс массы тела: " + bodyMassIndex);
+    }
 }
--- src/ConvertFeetToMeters.java
+++ src/ConvertFeetToMeters.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class ConvertFeetToMeters {
+import java.util.Scanner;
+
+public class ConvertFeetToMeters {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Введите число в футах: ");
+        double feet = input.nextDouble();
+
+        final double METERS_OF_FEET = 0.305;
+        double meters = feet * METERS_OF_FEET;
+        System.out.println("Метры: " + meters);
+    }
 }
